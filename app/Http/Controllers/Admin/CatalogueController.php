c8ccd0754b2a3f668af2b1186b7000900a1882c5
Awais Ahmad <awaisazeem287@gmail.com>
2024-01-06 16:45:20 +0500

    store the catalogue data

--- app/Http/Controllers/Admin/CatalogueController.php
+++ app/Http/Controllers/Admin/CatalogueController.php
@@ -5,8 +5,17 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Helper\DzHelper;
 use App\Models\Catalogue;
+
+use App\Models\Product;
+use App\Models\Blog;
+use App\Models\BlogCategory;
+use App\Models\BlogBlogCategory;
+use App\Models\BlogTag;
+use App\Models\BlogBlogTag;
+use App\Models\BlogMeta;
+use App\Models\BlogSeo;
 use App\Models\User;
-// use App\Rules\EditorEmptyCheckRule;
+use App\Rules\EditorEmptyCheckRule;
 use Storage;
 
 class CatalogueController extends Controller
@@ -31,7 +40,15 @@ class CatalogueController extends Controller
      */
     public function create()
     {
-        //
+        $page_title = __('Add New Catalogue');
+        $catalogues = Catalogue::get();
+        $users = User::get();
+        $exhibitions = Blog::get();
+        $screenOption = config('exhibition.ScreenOption');
+        $categoryArr = (new BlogCategory())->generateCategoryTreeListCheckbox(Null, ' ');
+        $parentCategoryArr = (new BlogCategory())->generateCategoryTreeArray(Null, '&nbsp;&nbsp;&nbsp;');
+        //dd($screenOption);
+        return view('admin.catalogue.create', compact('users', 'exhibitions', 'categoryArr', 'parentCategoryArr', 'page_title', 'screenOption'));
     }
 
     /**
@@ -42,7 +59,51 @@ class CatalogueController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $validation = [
+            'data.Catalogue.title'           => 'required',
+            'data.Catalogue.content'         => ['required', new EditorEmptyCheckRule],
+            'data.Catalogue.slug'            => 'unique:blogs,slug',
+            'data.Catalogue.publish_on'      => 'required',
+            'data.CatalogueMeta.0.value'     => 'mimes:jpg,png,jpeg,gif',
+        ];
+
+        $validationMsg = [
+            'data.Catalogue.title.required'      => __('The title field is required.'),
+            'data.Catalogue.content.required'    => __('The exhibition content field is required.'),
+            'data.Catalogue.publish_on.required' => __('The published on field is required.'),
+            'data.Catalogue.slug.unique'         => __('The slug has already been taken.'),
+            'data.CatalogueMeta.0.value.mimes'   => __('The feature image must be a file of type: jpg, png, jpeg, gif.'),
+        ];
+        
+        $this->validate($request, $validation, $validationMsg);
+        $CatalogueData   = $request->input('data.Catalogue');
+        $CatalogueData['user_id'] = $request->input('data.Catalogue.user_id') ? $request->input('data.Catalogue.user_id') : Auth::id();
+        $catalogue       = Exhibition::create($CatalogueData);
+        $catalogue_metas = collect($request->data['CatalogueMeta'])->sortKeys()->all();
+        $catalogueTag  = !empty($request->input('data.ExhibitionTag')) ? explode(',', $request->input('data.ExhibitionTag')) : '';
+
+        if($catalogue)
+        {
+            
+
+            //DzHelper::saveFile($request,Exhibition::class,$catalogue,'uploads/configuration-images','CatalogueMeta');
+            $result = $this->__imageSave($request, $catalogue, 'CatalogueMeta');
+            if(count($result)>0){
+                foreach ($result as $catalogueId => $imageFilename) {
+                    $cataloguerecord = Catalogue::find($catalogueId);
+                    if ($cataloguerecord) {
+                        $catalogue->featured_id = $catalogueId;
+                        $catalogue->featured_image = $imageFilename;
+                        $catalogue->update();
+                    }
+                }
+            }
+            //dd($result);
+            // need to update the record of exhibition of created  $exhibition->id
+            return redirect()->route('catalogue.admin.index')->with('success', __('Catalogues added successfully.'));
+
+        }
+        return redirect()->back()->with('error', __('Something went wrong. Please try again.'));
     }
 
     /**
